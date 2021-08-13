cb5c8f4136638fb7a714072d5a71eb7e1758f8d8
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-13 13:58:07 +0530

    Add files via upload

---
+++ dockerfile
@@ -0,0 +1,14 @@
+# Container image that runs your code
+FROM python:3
+
+COPY Qualys_IaC_Security-1.3.2-py3-none-any.whl /Qualys_IaC_Security-1.3.2-py3-none-any.whl
+COPY entrypoint.sh /entrypoint.sh
+
+RUN pip3 install --upgrade pip
+RUN pip3 install click requests prettytable click_option_group
+RUN pip3 install Qualys_IaC_Security-1.3.2-py3-none-any.whl
+
+RUN ["chmod", "+x", "/entrypoint.sh"]
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+
+#echo "action_state=`git log -n 2 --pretty=format:"%H" | tail -1`" >> $GITHUB_ENV
+#git diff --name-only --diff-filter=ACMRT ${{ github.sha }} ${{ env.action_state }}
+
+rm -rf ./scan
+
+mkdir scan
+
+#validate
+find . -name \*.tf -o -name  \*.template -o -name \*.yml -o -name \*yaml -o -name \*.json | xargs -I '{}' cp {} ./scan
+
+#call cli
+qiac scan -a $URL -u $UNAME -p $PASS -d ./scan --format json -n charchiltagtest --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"}] > result.json
+
+ls -ltr
+
+cat result.json
+
+#process result for annotation
+python pythonscript.py result.json
+
+
+
+
+
+
+
+
+
