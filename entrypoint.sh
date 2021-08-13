61af9728361d0ee7093509b1fa3e71caeff46078
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-13 14:58:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,23 @@
-#!/bin/sh -l
+#!/bin/sh
+
+echo "charchil"
 
 #echo "action_state=`git log -n 2 --pretty=format:"%H" | tail -1`" >> $GITHUB_ENV
 #git diff --name-only --diff-filter=ACMRT ${{ github.sha }} ${{ env.action_state }}
 
-rm -rf ./scan
+#rm -rf ./scan
 
-mkdir scan
+#mkdir scan
 
 #validate
-find . -name \*.tf -o -name  \*.template -o -name \*.yml -o -name \*yaml -o -name \*.json | xargs -I '{}' cp {} ./scan
+#find . -name \*.tf -o -name  \*.template -o -name \*.yml -o -name \*yaml -o -name \*.json | xargs -I '{}' cp {} ./scan
 
 #call cli
-qiac scan -a $URL -u $UNAME -p $PASS -d ./scan --format json -n charchiltagtest --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"}] > result.json
+#qiac scan -a $URL -u $UNAME -p $PASS -d ./scan --format json -n charchiltagtest --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"}] > result.json
 
 ls -ltr
 
-cat result.json
+#cat result.json
 
 #process result for annotation
-python pythonscript.py result.json
+#python pythonscript.py result.json
