babb273e3d22876b8ec1b804e0312702a2d7ff08
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-13 15:13:49 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,17 @@
 #/bin/sh -l
-echo "charchilgupta"
+rm -rf ./scan
+
+mkdir scan
+
+#validate
+find . -name \*.tf -o -name  \*.template -o -name \*.yml -o -name \*yaml -o -name \*.json | xargs -I '{}' cp {} ./scan
+
+#call cli
+qiac scan -a $URL -u $UNAME -p $PASS -d ./scan --format json -n charchiltagtest --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"}] > result.json
+
+ls -ltr
+
+cat result.json
+
+#process result for annotation
+python pythonscript.py result.json
