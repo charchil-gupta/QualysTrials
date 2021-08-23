6e95515a5ef5c2ea4cfb299b5e305636b9b65dac
charchil-gupta <64839926+charchil-gupta@users.noreply.github.com>
2021-08-23 12:18:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,12 @@ then
     cp --parents $(git diff --name-only --diff-filter=ACMRT HEAD^ HEAD) scanfolder
     cd scanfolder
     echo "Scanning Started at - $(date +"%Y-%m-%d %H:%M:%S")"
-    qiac scan -a $URL -u $UNAME -p $PASS -d $SCANFOLDER --format json -n GitHubActionScan --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"},{\"REPOSITORY_NAME\":\"$GITHUB_REPOSITORY\"}] > ./result.json
+    qiac scan -a $URL -u $UNAME -p $PASS -d $SCANFOLDER --format json -n GitHubActionScan --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"},{\"REPOSITORY_NAME\":\"$GITHUB_REPOSITORY\"}] > result.json
     echo "Scanning Completed at - $(date +"%Y-%m-%d %H:%M:%S")"
     #process result for annotation
     echo " "
     echo "\e[4mSCAN RESULT\e[0m"
+    mv result.json /result.json
     cd /
     ls -ltr 
     python resultParser.py result.json
@@ -32,13 +33,14 @@ else
 
     #Calling Iac CLI
     echo "Scanning Started at - $(date +"%Y-%m-%d %H:%M:%S")"
-    qiac scan -a $URL -u $UNAME -p $PASS -d $SCANFOLDER --format json -n GitHubActionScan --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"},{\"REPOSITORY_NAME\":\"$GITHUB_REPOSITORY\"}] > ./result.json
+    qiac scan -a $URL -u $UNAME -p $PASS -d $SCANFOLDER --format json -n GitHubActionScan --tag [{\"BRANCH_NAME\":\"$GITHUB_REF\"},{\"REPOSITORY_NAME\":\"$GITHUB_REPOSITORY\"}] > result.json
     echo "Scanning Completed at - $(date +"%Y-%m-%d %H:%M:%S")"
     #process result for annotation
     echo " "
     echo "\e[4mSCAN RESULT\e[0m"
+    mv result.json /result.json
     cd /
-     ls -ltr 
+    ls -ltr 
     python resultParser.py result.json
 fi
 
